4461e79adf8432076c44531592442744af6fd75b
Md Shoyaib Islam <shoyaib105@gmail.com>
2026-04-10 10:42:19 +0600

    save connet

--- server/models/Message.js
+++ server/models/Message.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 
-const messageSchema = new mongoose.Schema({
-  text: String,
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  avatar: String,
 });
 
-module.exports = mongoose.model("Message", messageSchema);
+module.exports = mongoose.model("User", userSchema);
